02cf8e00f440311ae53bec3386068b6c064a5ff8
Hardik Sharma <sharmahardikaz@gmail.com>
2023-12-18 14:36:09 +0530

    Finished

--- vite-project/src/AuthPage.jsx
+++ vite-project/src/AuthPage.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import axios from 'axios'
 
 const AuthPage = (props) => {
     const onSubmit = (e) => {
+
       e.preventDefault();
       const { value } = e.target[0];
-      props.onAuth({ username: value, secret: value });
+
+      axios.post('http://localhost:3001/authenticate',
+      {username: value})
+      .then(r => props.onAuth({...r.data, secret: value}))
+      .catch(e => console.log('error',e))
+
+      // props.onAuth({ username: value, secret: value });
     };
   
     return (
--- vite-project/src/ChatsPage.jsx
+++ vite-project/src/ChatsPage.jsx
@@ -1,7 +1,15 @@
 import './App.css'
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic('bc761a89-6c78-4c9f-85d6-07960c121dbc',props.user.username,
+     props.user.secret)
+    return <div style={{ height: '100vh'}}>
+    <MultiChatSocket {...chatProps} />
+    <MultiChatWindow {...chatProps} style={{ height: "100%"}} />
 
-const ChatsPage = () => {
-    return <>Chats...</>
+    </div>
+
+
 }
 
 export default ChatsPage
